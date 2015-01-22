0008017c2e9da9ab3d2961b03f54d854dd39b52f
Andrei Arsenin <dread@deimos.li>
2015-01-22 23:51:41 +0200

    Added Russian locale

---
+++ locales/ru.js
@@ -0,0 +1,51 @@
+SirTrevor.Locales.ru = {
+  general: {
+    'delete':           'Удалить?',
+    'drop':             'Перетащите __block__ сюда',
+    'paste':            'Или вставьте ссылку сюда',
+    'upload':           '...или выберите файл',
+    'close':            'закрыть',
+    'position':         'Расположение',
+    'wait':             'Пожалуйста, подождите...',
+    'link':             'Введите ссылку'
+  },
+  errors: {
+    'title': "Возникли следующие ошибки:",
+    'validation_fail': "Ошибка в блоке __type__",
+    'block_empty': "Поле не должно быть пустым",
+    'type_missing': "Необходим хотя бы один блок __type__",
+    'required_type_empty': "Обязательный блок __type__ пуст",
+    'load_fail': "Возникла ошибка при загрузке содержимого документа"
+  },
+  blocks: {
+    text: {
+      'title': "Текст"
+    },
+    list: {
+      'title': "Список"
+    },
+    quote: {
+      'title': "Цитата",
+      'credit_field': "Автор"
+    },
+    image: {
+      'title': "Изображение",
+      'upload_error': "Не удалось загрузить изображение"
+    },
+    video: {
+      'title': "Видео"
+    },
+    tweet: {
+      'title': "Твит",
+      'fetch_error': "Не удалось загрузить твит"
+    },
+    embedly: {
+      'title': "Embedly",
+      'fetch_error': "Не удалось загрузить содержимое блока",
+      'key_missing': "Ключ Embedly API должен быть указан"
+    },
+    heading: {
+      'title': "Заголовок"
+    }
+  }
+};
